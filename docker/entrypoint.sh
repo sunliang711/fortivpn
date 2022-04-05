142177ee85de27300cf7e633b64d385e7df787df
sunliang <sunliang711@163.com>
2022-04-05 16:33:40 +0800

    ip firewall

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,8 +11,20 @@ interact
 
 EOF
 
-tmux new-session -s vpn -d "/usr/bin/expect -f ${vpnScript}||bash"
 
+if [[ ${hsmOn} == "yes" ]];then
+    tmux new-session -s vpn -d "/usr/bin/expect -f ${vpnScript} || bash"
+
+    while ! ip link show ppp0;do
+        echo 'no ppp0'
+        sleep 2
+    done
+
+	ip route del default dev ppp0
+	ip route add 192.168.10.0/24 dev ppp0
+	ip route add default via 172.17.0.1 dev eth0
+
+fi
 
 #TODO: replace your real service
 python3 -m http.server 80
